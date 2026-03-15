d30137013ce0f09f609a6fad10b21c0a5d0947b1
Ayush Mohanty <ayushmohanty5800@gmail.com>
2026-03-15 07:38:21 +0530

    Add CV download button

--- src/components/AnimatedBoarderButton.jsx
+++ src/components/AnimatedBoarderButton.jsx
@@ -1,14 +1,12 @@
+import { ArrowRight,Download } from "lucide-react"
+
+
 export const AnimatedBorderButton = () => {
     return(
-       <a 
-        href="c:\Ayush_Mohanty_CV (2).pdf"
-        download="\Ayush_Mohanty_CV (2).pdf"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="group relative bg-transparent shadow-[0_0_25px_rgba(32,178,166,0.6)]
+       <button className="group relative bg-transparent shadow-[0_0_25px_rgba(32,178,166,0.6)]
        text-foreground hover:text-primary transition-all duration-300
        focus:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2
-       px-8 py-4 text-lg font-medium rounded-full overflow-visible inline-block">
+       disabled:opacity-50 disabled:cursor-not-allowed px-8 py-4 text-lg font-medium rounded-full overflow-visible">
                             {/* Animated svg border */}
                             <svg 
                             className="absolute left-0 top-0 w-full h-full pointer-events-none"
@@ -25,11 +23,14 @@ export const AnimatedBorderButton = () => {
                                 strokeDashoffset="round"
                                 strokeLinecap="round"
                                 className="animation-border-path"/>
+
                             </svg>
                             <span className="flex items-center gap-2 relative z-10">
                                 <Download className="w-5 h-5"/>
                                 Download CV
                             </span>
-                        </a>
+                        </button>
+                   
     )
 }
+
